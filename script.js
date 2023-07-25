1c63272b96549e3076f426d97be663ef317a1621
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-07-25 22:23:15 +0530

    feat:add  matching and empty password validation

--- script.js
+++ script.js
@@ -5,23 +5,37 @@ PasswordDiv = document.querySelector('.Password-div');
 password.addEventListener('input', handlePassword);
 confirm_password.addEventListener('input', handlePassword);
 
-errorText = document.createElement("p");
-errorText.textContent = '*Passwords do not match';
-errorText.classList.add('PasswordMatchingError')
+invalidText = document.createElement("p");
+invalidText.textContent = '*Passwords do not match';
+invalidText.classList.add('invalidPassword')
+
+validText = document.createElement("p");
+validText.textContent = '*Passwords match';
+validText.classList.add('validPassword')
 
 function handlePassword(e) {
+  
+  if (!password.value || !confirm_password.value) { 
+    if(PasswordDiv.contains(invalidText)) PasswordDiv.removeChild(invalidText);
+    if(PasswordDiv.contains(validText)) PasswordDiv.removeChild(validText);
+    
+    password.classList.remove('invalidInput');
+    confirm_password.classList.remove('invalidInput');
+    this.classList.add('input-focus');
+  }
   if (password.value !== confirm_password.value) {
-    password.classList.add('error');
-    confirm_password.classList.add('error');
-    PasswordDiv.appendChild(errorText);
+    password.classList.add('invalidInput');
+    confirm_password.classList.add('invalidInput');
+    PasswordDiv.appendChild(invalidText);
+    PasswordDiv.removeChild(validText);
   }
   
   if (password.value === confirm_password.value) {
-    
-    password.classList.remove('error');
-    confirm_password.classList.remove('error');
+    password.classList.remove('invalidInput');
+    confirm_password.classList.remove('invalidInput');
+    PasswordDiv.removeChild(invalidText);
+    PasswordDiv.appendChild(validText);
     this.classList.add('input-focus');
-    PasswordDiv.removeChild(errorText);
   }
   
 }
